b5fa8d093ebca8ab96b0ad011cb0547c282f9fe5
Aram Valcourt <115132116+AramCodes@users.noreply.github.com>
2023-10-06 23:28:09 -0400

    finished  search param challenge
    do challenges in vans.jsx tomorrow

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# coddc4b549fee03954bcad58f
+# co4ac4e36b2a4badc8d5896a6
 
 Warning: Vite enforces using jsx syntax inside jsx/tsx files, so it will complain about that. Solution: rename `.js` files to `.jsx` :)
 
--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -1,15 +1,23 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import { Link, useSearchParams } from "react-router-dom"
 
 export default function Vans() {
+    const [searchParams, setSearchParams] = useSearchParams()
     const [vans, setVans] = React.useState([])
+    
+    const typeFilter = searchParams.get("type")
+    
     React.useEffect(() => {
         fetch("/api/vans")
             .then(res => res.json())
             .then(data => setVans(data.vans))
     }, [])
+    
+    const displayedVans = typeFilter
+        ? vans.filter(van => van.type === typeFilter)
+        : vans
 
-    const vanElements = vans.map(van => (
+    const vanElements = displayedVans.map(van => (
         <div key={van.id} className="van-tile">
             <Link to={`/vans/${van.id}`}>
                 <img src={van.imageUrl} />
@@ -21,10 +29,27 @@ export default function Vans() {
             </Link>
         </div>
     ))
+    
+    /**
+     * Challenge: add links to filter the vans by type. Use a hard-coded
+     * `to` string like we just practiced. The types are "simple", 
+     * "luxury", and "rugged".
+     * 
+     * For now, give the Links a className of `van-type simple` (and
+     * manually replace "simple" with "luxury" and "rugged" for 
+     * the Links that filter by those types.)
+     * 
+     * Include a Link to clear the filters. Its className should be
+     * `van-type clear-filters`
+     */
 
     return (
         <div className="van-list-container">
             <h1>Explore our van options</h1>
+            <div className="van-list-filter-buttons">
+            
+            
+            </div>
             <div className="van-list">
                 {vanElements}
             </div>
